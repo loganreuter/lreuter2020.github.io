48c01659df6b3aaf193ad0227bed718e47272f01
lreuter2020 <lreuter2020@gmail.com>
2023-05-20 20:27:07 -0700

    deployed

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+npm run build
+
+git add -A
+git commit -m "deployed"
+git push origin main
+
+cd dist
+echo "www.loganreuter.com" >> CNAME
+git init -b deploy
+git add -A
+git commit -m "deployed"
+git remote add origin https://github.com/lreuter2020/lreuter2020.github.io.git
+git push -f origin deploy
+cd -
